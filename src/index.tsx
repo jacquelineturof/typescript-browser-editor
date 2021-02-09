b9a15484eb62800a076fcc89d5b649881fcd7269
jacquelineturof <jacquelineturof@gmail.com>
2021-02-09 14:10:55 -0500

    created preview component

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
+
 import CodeEditor from './components/CodeEditor'
+import Preview from './components/Preview'
 
 const App = () => {
     const ref = useRef<any>()
-    const iframe = useRef<any>()
+    const [ code, setCode ] = useState('')
     const [ input, setInput ] = useState('')
 
     useEffect(() => {
@@ -25,7 +27,7 @@ const App = () => {
     const onClick = async () => {
         if (!ref.current) return
 
-        iframe.current.srcdoc = html
+        
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -38,52 +40,20 @@ const App = () => {
             }
         })
 
-        // setCode(result.outputFiles[0].text)
-        iframe.current.contentWindow.postMessage(
-            result.outputFiles[0].text, '*')
+        setCode(result.outputFiles[0].text)
     }
-
-    const html = `
-        <html>
-            <head>
-            </head>
-            <body>
-                <div id = "root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try {
-                            eval(event.data)
-                        } catch (e) {
-                            const root = document.querySelector('#root')
-                            root.innerHTML = '<div style = "color: red;"><h4>Runtime Error</h4>' + e + '</div>'
-                            console.error(e)
-                        }
-                    }, false)
-                </script>
-            </body>
-        </html>
-    `
 
     return (
         <div>
             <CodeEditor 
                 initialValue = "const a = 1;"
                 onChange = { (value) => setInput(value)  } />
-            <textarea 
-                value = { input } 
-                onChange = { e => setInput(e.target.value) }>
-
-            </textarea>
             <div>
                 <button onClick = { onClick }>
                     Submit
                 </button>
             </div>
-            <iframe
-                title = "preview"
-                ref = { iframe } 
-                sandbox = "allow-scripts" 
-                srcDoc = { html } />
+            <Preview code = { code } />
         </div>
     )
 }
