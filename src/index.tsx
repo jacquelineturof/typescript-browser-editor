6a0267daaee6ca79bd6fb654c76447348a9f1c39
jacquelineturof <jacquelineturof@gmail.com>
2021-02-16 15:04:29 -0500

    added markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 import ReactDOM from 'react-dom'
 
-import CodeCell from './components/CodeCell'
+// import CodeCell from './components/CodeCell'
+import TextEditor from './components/TextEditor'
 
 const App = () => {
     return (
         <div>
-            <CodeCell />
+            <TextEditor />
         </div>
     )
 }
