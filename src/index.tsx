115a56e8a71bf9a0474f4a2e580668dffecb0469
jacquelineturof <jacquelineturof@gmail.com>
2021-02-18 15:24:25 -0500

    working on cellsreducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './state'
 
 // import CodeCell from './components/CodeCell'
 import TextEditor from './components/TextEditor'
 
 const App = () => {
     return (
-        <div>
-            <TextEditor />
-        </div>
+        <Provider store = { store }>
+            <div>
+                <TextEditor />
+            </div>
+        </Provider>
     )
 }
 
