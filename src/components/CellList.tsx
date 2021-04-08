973dc1cec7ce219814ffe28001d802addf2a23c2
jacquelineturof <jacquelineturof@gmail.com>
2021-04-08 09:57:13 -0400

    adding some extra styling to cell list

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { useTypedSelector } from '../hooks/use-typed-selector'
 import CellListItem from './CellListItem'
 import AddCell from './AddCell'
+import './CellList.css'
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: {  order, data }}) => {
@@ -16,7 +17,7 @@ const CellList: React.FC = () => {
     ))
         
     return (
-        <div>
+        <div className = "cell-list">
             <AddCell 
                 forceVisible = { cells.length === 0} 
                 previousCellId = { null } />
